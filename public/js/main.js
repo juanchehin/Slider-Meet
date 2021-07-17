9e5e05a90bc2f04f221c4d2580386fab45c9cc6a
chehin <chehinjuan@gmail.com>
2021-07-17 12:02:44 -0300

    Cambio de logo

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,9 @@
+import * as store from './store.js';
+
 const socket = io("/");
 // import { io } from "../../app.js";
 
 socket.on("connect", () => {
     console.log(" conexion exitosa con wss server");
-    console.log(socket.id);
+    store.setSocketId(socket.id);
 })
---
+++ public/js/store.js
@@ -0,0 +1,56 @@
+// Estado inicial
+let state = {
+    socketId: null,
+    localStream: null,
+    remoteStream: null,
+    screenSharingActive: false,
+    screenSharingStream: null,
+    allowConnectionsFromStrangers: false,
+}
+
+export const setSocketId = (socketId) => {
+    state = {
+        ...state,
+        socketId,
+    };
+    console.log(state);
+};
+
+export const setLocalStream = (stream) => {
+    state = {
+        ...state,
+        localStream: stream
+    }
+}
+
+export const setAllowConnectionsFromStrangers = (allowConnection) => {
+    state = {
+        ...state,
+        allowConnectionsFromStrangers: allowConnection
+    }
+}
+
+export const setScreenSharingActive = (screenSharingActive) => {
+    state = {
+        ...state,
+        screenSharingActive,
+    };
+};
+
+export const setScreenSharingStream = (stream) => {
+    state = {
+        ...state,
+        remoteStream: stream,
+    };
+};
+
+export const setRemoteStream = (stream) => {
+    state = {
+        ...state,
+        remoteStream: stream,
+    };
+};
+
+export const getState = (stream) => {
+    return state;
+};
